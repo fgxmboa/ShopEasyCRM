4e2ed7b84392fc4536297d1fc30e312fae9ddb12
elifg1 <floeli07@gmail.com>
2025-10-21 17:39:16 -0600

    Eliza

---
+++ ShopEasyCRM/Controllers/CuentaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopEasyCRM.Models;
+using System.Linq;
+
+namespace ShopEasyCRM.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+
+    public class CuentaController : Controller
+    {
+        private readonly ShopEasyContext _context;
+
+        public CuentaController(ShopEasyContext context)
+        {
+            _context = context;
+        }
+
+        // GET y POST combinados
+        [HttpGet, HttpPost]
+        public IActionResult Login(string email = null, string password = null)
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                // Buscar usuario
+                var usuario = _context.Usuarios
+                    .FirstOrDefault(u => u.Email == email && u.Password == password);
+
+                if (usuario != null)
+                {
+                    // Usuario válido, redirige directo al Home
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Error = "Email o contraseña incorrecta";
+                }
+            }
+
+            return View();
+        }
+    }
+}
